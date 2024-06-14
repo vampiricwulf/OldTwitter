5c0aaf72edcd7803f6501efb178e808473420d4a
Wulf <turk.brandon@gmail.com>
2024-06-13 18:31:38 -0700

    Replace download template

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -3538,8 +3538,8 @@ async function appendTweet(t, timelineContainer, options = {}) {
 
                         let ts = new Date(t.created_at).toISOString().split("T")[0];
                         let extension = url.pathname.split('.').pop();
-                        let _index = t.extended_entities.media.length > 1 ? "_"+(index+1) : "";
-                        let filename = `${t.user.screen_name}_${ts}_${t.id_str}${_index}.${extension}`;
+                        let _index = t.extended_entities.media.length > 1 ? "-"+(index) : "";
+                        let filename = `@${t.user.screen_name}-${t.id_str}${_index}.${extension}`;
                         a.download = filename;
 
                         a.click();
