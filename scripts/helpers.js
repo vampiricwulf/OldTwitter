2f929c15bd60b9efd1f3271316e85d940e242c10
Wulf <turk.brandon@gmail.com>
2024-07-16 09:25:48 -0700

    hyphenate index and use preferred default

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -3593,8 +3593,8 @@ async function appendTweet(t, timelineContainer, options = {}) {
 
                         let ts = new Date(t.created_at).toISOString().split("T")[0];
                         let extension = url.pathname.split('.').pop();
-                        let _index = t.extended_entities.media.length > 1 ? "_"+(index+1) : "";
-                        let filename = `${t.user.screen_name}_${ts}_${t.id_str}${_index}.${extension}`;
+                        let _index = t.extended_entities.media.length > 1 ? "-"+(index+1) : "";
+                        let filename = `${t.user.screen_name}-${t.id_str}${_index}.${extension}`;
                         let filename_template = vars.customDownloadTemplate;
 
                         // use the filename from the user's custom download template, if any
